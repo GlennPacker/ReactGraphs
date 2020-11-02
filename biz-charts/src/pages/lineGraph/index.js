99cb4b7f879d04632de1f6fb4f7ff97a1975af49
glennpacker <glenn.packer@branthill.co.uk>
2020-11-02 11:26:23 +0000

    basic pages for pie and line charts with biz-charts, nivo and victory

---
+++ biz-charts/src/pages/lineGraph/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LineGraph = () => {
+    return (
+        <div>
+            <h2>Line Graph</h2>
+
+        </div>
+    );
+}
+
+export default LineGraph;
---
+++ nivo-charts/src/app/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Nav from '../components/nav/Nav';
+import Routes from './Routes';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>
+                Nivo Charts
+            </h1>
+            <div style={{ display: 'flex', flexDirection: 'row'}} >
+                <div style={{ flex: 'none', width: '10em' }} >
+                    <Nav />
+                </div>
+                <div style={{ flexGrow: 1, textAlign: 'left' }}>
+                    <Routes />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
