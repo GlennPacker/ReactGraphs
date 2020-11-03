c9149b4011800220b4743d7806e1fab9c8438876
glennpacker <glenn.packer@branthill.co.uk>
2020-11-03 11:56:26 +0000

    nivo line graph style adjustment

--- nivo-charts/src/pages/lineGraph/lineGraph.js
+++ nivo-charts/src/pages/lineGraph/lineGraph.js
@@ -8,9 +8,10 @@ export const LineGraph = ({ data, yFormat = null, title = 'Power'  }) => (
         data={data}
         margin={{ top: 50, right: 150, bottom: 50, left: 50 }}
         xScale={{ type: 'point' }}
-        yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+        yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: false, reverse: false }}
+        enableSlices="x"
         yFormat
-        curve="cardinal"
+        curve="monotoneX"
         axisTop={null}
         axisRight={null}
         axisBottom={{
